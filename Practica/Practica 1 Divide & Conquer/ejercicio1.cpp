856e86ad0b15a04b6aa6ed2d44aa3063b2d7d472
Matias258 <ganguimatias@gmail.com>
2025-08-21 10:04:58 -0300

    ds

--- Practica/Practica 1 Divide & Conquer/ejercicio1.cpp
+++ Practica/Practica 1 Divide & Conquer/ejercicio1.cpp
@@ -35,7 +35,7 @@ def merge ( izq , der ):
     return mergeados
 
 */
-// Esta haciendo mal el merge
+
 vector<int> merge (vector<int> izq, vector<int> der){
     vector<int> mergeados;
     int i, j = 0;
@@ -46,7 +46,7 @@ vector<int> merge (vector<int> izq, vector<int> der){
             i++;
         }
         else {
-            mergeados.push_back(der[i]);
+            mergeados.push_back(der[j]);
             j++; 
         }
         
@@ -65,21 +65,24 @@ vector<int> merge_sort(vector<int> arr){
     if (arr.size() <= 1){
         return arr;
     }
-    int medio = arr.size(); 
+    int medio = arr.size() / 2;                   // Divide
     vector<int>mitad_izq;
     vector<int>mitad_der; 
-    for (int k = 0; k < arr.size() / 2; k++){
+    for (int k = 0; k < medio; k++){
         mitad_izq.push_back(arr[k]);
     }
-    for (int k = arr.size() / 2; k < arr.size(); k++){
+    for (int k = medio; k < arr.size(); k++){
         mitad_der.push_back(arr[k]);
     }
-    return merge(mitad_izq, mitad_der);
+    mitad_izq = merge_sort(mitad_izq);            // Conquer
+    mitad_der = merge_sort(mitad_der);            // Conquer
+
+    return merge(mitad_izq, mitad_der);           // Combine
 }
 
 int main(){
 
-    vector<int> arr = {1, 6, 2, 2, 8, 34};
+    vector<int> arr = {1, 6, 2, 2, 8, 433 , 1 , -54};
     vector<int> sorted_arr = merge_sort(arr);
 
     cout << "The Sorted array elements are: ";
@@ -88,3 +91,60 @@ int main(){
     }
     return 0;
 }
+
+/*
+Respondamos a las preguntas del enunciado:
+1. Identificar qu´e lineas son el divide, cu´ales son el conquer y cu´ales el combine.
+2. ¿En cu´antos subproblemas se divide?
+3. ¿De qu´e tama˜no son estos subproblemas?
+4. ¿Cu´al es el costo de combinar los resultados de los subproblemas?
+5. Escribir la funci´on T(n) de manera recursiva.
+6. Determinar la complejidad del algoritmo utilizando el Teorema Maestro.
+
+1) 
+a. Divide: dividimos el problema por la mitad
+int medio = arr.size() / 2;  
+
+b. Conquer: Resuelve los problemas más pequeños recursivamente
+mitad_izq = merge_sort(mitad_izq);
+mitad_der = merge_sort(mitad_der);
+
+c. Combine: Reordena los problemas más pequeños
+return merge(mitad_izq, mitad_der); 
+
+2,3) Se divide en 2 subproblemas de tamaño n/2, mitad_izq y mitad_der
+
+4) El costo de combinar los resultados de los subproblemas es de O(n):
+a. La funcion merge recorre cada elemento una sola vez
+b. En el peor caso, compara todos los elementos de ambas mitades
+c. Total de operaciones: n comparaciones + n inserciones = O(n)
+
+5) Recordemos el Teo Maestro:
+T(n) = { 1              ,si n = 1 }
+       { aT(n/c) + f(n) ,si n > 1 }
+(1) Si f(n) = O(n^logc(a)-E) para E > 0, entonces T(n) = Tita(n^logc(a))
+(2) Si f(n) = Tita(n^logc(a)), entonces T(n) = Tita(n^logc(a)) * log(n))
+(3) Si f(n) = Omega(n^logc(a)+E) para E > 0 y af(n/c) < kf(n) para k < 1 y n suficientemente grandes, entonces T(n) = Tita(f(n))       
+
+Donde:
+a: Numero de subproblemas
+c: factor de división 
+f(n): Costo de Combinar (Merge)
+
+Veamos para Merge Sort
+Tenemos que:
+a = 2, tenemos 2 iteraciones cada vez
+c = 2, particionamos el problema en 2 partes
+f(n) = Tita(n) (Merge) = Tita(n), pues SABEMOS que la combinación es lineal (en cota sup e inf).
+                         Si usamos O(n), vamos a tener distintos resultados.
+
+Entonces 
+T(n) = { Tita(1)                ,si n <= 1 }
+       { 2T(n/2) + Tita(n)      ,si n > 1  }
+
+6. Veamos que dice el Teorema Maestro en este caso:
+Como f(n) = Tita(n), entramos en el 2do caso, pues Tita(n) ⊂ Tita(n^log2(2)) = Tita(n)
+--> Tita(n) ⊂ Tita(n)
+Entonces obtenemos que Merge Sort tiene T(n) = Tita(n^log2(2)) * log(n)) = Tita(n * log(n))
+Merge Sort tiene complejidad Tita(nlog(n))
+*/
