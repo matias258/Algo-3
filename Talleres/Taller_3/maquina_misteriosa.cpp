c79d6bfb58b4299c2d8d5f2a48142c55f285594d
Matias258 <ganguimatias@gmail.com>
2025-10-23 17:20:50 -0300

    dfgds

--- Talleres/Taller_3/maquina_misteriosa.cpp
+++ Talleres/Taller_3/maquina_misteriosa.cpp
@@ -9,4 +9,53 @@
 #include <algorithm>
 #include <bits/stdc++.h>
 #include <utility>
+#include <math.h>
 using namespace std;
+
+// Queremos encontrar el minimo camino de n a m, esto grita a 4 vientos BFS
+// Rdo: BFS me devuelve la distancia minima entre 2 nodos
+// En nuestro caso las distancias van a ser todas = 1, pues es la cantidad de veces que aprieto el boton
+// Los nodos serán el valor mostrado en la pantalla en ese momento dado
+int bfs(int n, int m) {
+    if (n == m) return 0;
+
+    const int MAX = 1000000;
+    vector<bool> visitado(MAX + 1, false); // Nodos no visitados
+    queue<pair<int, int>> nodo;           // {nodo_actual, pasos}
+
+    nodo.push(make_pair(n, 0));           // Inicializamos en n con 0 pasos
+    visitado[n] = true;                   
+
+    while (!nodo.empty()) {
+        int actual = nodo.front().first;  
+        int paso = nodo.front().second;  
+        nodo.pop();
+
+        // Rojo -> n = n * 2
+        long long rojo = (long long)actual * 2;
+        if (rojo <= MAX && !visitado[rojo]) {
+            if (rojo == m) return paso + 1;
+            visitado[rojo] = true;
+            nodo.push(make_pair(rojo, paso + 1));
+        }
+
+        // Azul -> n = n - 1
+        int azul = actual - 1;
+        if (azul >= 1 && !visitado[azul]) {
+            if (azul == m) return paso + 1;
+            visitado[azul] = true;
+            nodo.push(make_pair(azul, paso + 1));
+        }
+    }
+
+    return -1; 
+}
+
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    cout << bfs(n, m) << endl;
+    return 0;
+}
